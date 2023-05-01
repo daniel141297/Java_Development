e114cab0724762a5b006ddd601d5d3a4a3ebf6c5
kewin8 <79927883+kewin8@users.noreply.github.com>
2023-05-01 11:56:29 +0530

    Add files via upload

---
+++ SpringBoot_RESTFulWebServices/src/main/java/com/springboot/service/SpringBootRestFulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestFulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestFulWebServicesApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_RESTFulWebServices/src/test/java/com/springboot/service/SpringBootRestFulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestFulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
